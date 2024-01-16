df4daf6b165a2f542dd7e2444e822c6dabc997bf
Kenny <75525014+KennySB-dev@users.noreply.github.com>
2024-01-16 15:51:54 +0000

    Update main.js
    removed non working contact me form thing

--- main.js
+++ main.js
@@ -28,22 +28,3 @@ function linkAction() {
 }
 
 $navLink.forEach((n) => n.addEventListener('click', linkAction))
-
-const $form = document.querySelector('#form')
-const $buttonMailTo = document.querySelector('#KennyMail')
-
-$form.addEventListener('submit', handleSubmit)
-
-// totally *cough* functional Contact Form
-function handleSubmit(event) {
-  event.preventDefault()
-  const form = new FormData(this)
-  $buttonMailTo.setAttribute(
-    'href',
-    `mailto:kenny@kenny.is-a.dev?subject= Site: ${form.get(
-      'name'
-    )} Correo: ${form.get('mail')} &body=${form.get('message')}`
-  )
-  // im stupid :D
-  $buttonMailTo.click()
-}
